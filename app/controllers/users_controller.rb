fa030e3238304f1ae8bcc572d1a585ae753fcacb
jdowns525 <downsjosseph@gmail.com>
2023-03-17 15:42:15 +0000

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,10 +22,18 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id )
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
+    else 
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
